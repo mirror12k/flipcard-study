b748c867c3a30f6695aa52c9aad70d6048bbe9e6
mirror <mirror12k@gmail.com>
2023-02-13 22:22:58 -0500

    - invaded public morals with file dropper permissions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,6 +24,39 @@ const Home = () => {
 	};
 
 
+	const readFileAsync = file => new Promise((resolve, reject) => {
+		let reader = new FileReader();
+		reader.onload = () => {
+			resolve(reader.result);
+		};
+		reader.onerror = reject;
+		reader.readAsText(file);
+	});
+
+	const dragHover = e => {
+		e.stopPropagation();
+		e.preventDefault();
+
+		if (e.type === 'dragover') {
+			e.target.className = 'drop-area over';
+		} else {
+			e.target.className = 'drop-area';
+		}
+	}
+
+	const onDrop = e => {
+		e.stopPropagation();
+		e.preventDefault();
+		setloading(true);
+		Promise.all([...e.dataTransfer.files].map(file => readFileAsync(file))).then(data => {
+			setloading(false);
+			// console.log('data:', data);
+			loadcontent(data[0]);
+		});
+	}
+
+
+
 
 	return (loading ? <div><h1>Loading...</h1></div> :
 		(content
@@ -36,7 +69,11 @@ const Home = () => {
 			</div>
 
 			: <div>
-				<h1>Yes</h1>
+				<h1>Drop file or pick a github url:</h1>
+			    <div className="drop-area"
+					onDrop={onDrop}
+					onDragOver={dragHover}
+					onDragLeave={dragHover}></div>
 				<input className="form-control" onChange={e => seturl(event.target.value)} />
 				<button className="btn btn-primary" onClick={doload}>Submit</button>
 			</div>)
