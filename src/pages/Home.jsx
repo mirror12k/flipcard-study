9a4d3e279954b56bff555c74a8ea9cdef831d977
mirror <mirror12k@gmail.com>
2023-02-13 20:56:19 -0500

    - default app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+
+
+const Home = () => {
+	return <div></div>;
+}
+export default Home
+
+
