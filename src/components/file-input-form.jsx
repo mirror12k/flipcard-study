e4c44854dcae15f8a0d51814cbfde6f6732872f7
Mirror <mirror12k@gmail.com>
2023-04-22 11:33:01 -0400

    - more vocab

--- src/components/file-input-form.jsx
+++ src/components/file-input-form.jsx
@@ -24,6 +24,7 @@ const FileInputForm = (props) => {
 		['example_cards/japanese-vocab-4.txt', 'Japanese Vocab #4: 濃い/羨ましい/場所'],
 		['example_cards/japanese-vocab-5.txt', 'Japanese Vocab #5: 厳しい/珍しい/淋しい'],
 		['example_cards/japanese-vocab-6.txt', 'Japanese Vocab #6: 論文/就活/段々'],
+		['example_cards/japanese-vocab-7.txt', 'Japanese Vocab #7: 久々/優秀/多分'],
 	];
 
 	const doloadbyurl = () => {
